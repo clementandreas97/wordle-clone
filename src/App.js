9bace39081fc30411a02997a44fb147a2a50ffe6
Christopher Clement Andreas <clement.andreas97@gmail.com>
2022-06-26 00:30:06 +0800

    feat: init board display; add css loader to webpack config

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import Reactfrom "react";
+// React
+import React, { useState } from "react";
+
+// Styles
+import "./index.css";
+
+const WORD_LENGTH = 5;
+
+const Line = ({ guess }) => {
+  const tiles = [];
+  for (let i = 0; i < WORD_LENGTH; i++) {
+    const currChar = guess[i];
+    tiles.push(
+      <div key={i} className="tile">
+        {currChar}
+      </div>
+    );
+  }
+  return <div className="line">{tiles}</div>;
+};
 
 const App = () => {
+  const [guesses, setGuesses] = useState(Array(6).fill(null));
   return (
+    <>
       <div>Wordle</div>
+      <div className="tilesContainer">
+        {guesses.map((guess, idx) => {
+          return <Line key={idx} guess={guess ?? ""} />;
+        })}
+      </div>
+    </>
   );
 };
 
