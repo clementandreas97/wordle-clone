f281e403d9a6bb5cad5128e373953f4affcba41f
Christopher Clement Andreas <clement.andreas97@gmail.com>
2022-06-27 21:17:47 +0800

    feat: add game logic to compare with mock solution

--- src/App.js
+++ src/App.js
@@ -6,12 +6,22 @@ import "./index.css";
 
 const WORD_LENGTH = 5;
 
-const Line = ({ guess }) => {
+const Line = ({ guess, isEntered, solution }) => {
   const tiles = [];
   for (let i = 0; i < WORD_LENGTH; i++) {
     const currChar = guess[i];
+    let tileClass = "tile";
+    if (isEntered) {
+      if (currChar === solution[i]) {
+        tileClass += " correct";
+      } else if (solution.includes(currChar)) {
+        tileClass += " contains";
+      } else {
+        tileClass += " incorrect";
+      }
+    }
     tiles.push(
-      <div key={i} className="tile">
+      <div key={i} className={tileClass}>
         {currChar}
       </div>
     );
@@ -23,16 +33,38 @@ const App = () => {
   const [guesses, setGuesses] = useState(Array(6).fill(null));
   const [currentGuess, setCurrentGuess] = useState("");
   const [isFinished, setIsFinished] = useState(false);
+  // TODO: Change this to fetch/randomise from a set of words
+  const [solution, _] = useState("world");
 
   // Moving this outside of the useEffect, because it was capturing the
   // currentGuess when it is first initialised, which is empty.
   const handleType = (event) => {
+    console.log("handleType", currentGuess);
+    if (isFinished) {
+      return;
+    }
     const { key } = event || {};
     if (key === "Enter") {
       if (currentGuess.length !== WORD_LENGTH) return;
-      // TODO: Add check for solution
+      const newGuesses = [...guesses];
+      const insertIdx = newGuesses.findIndex((val) => val === null);
+      newGuesses[insertIdx] = currentGuess;
+      setGuesses(newGuesses);
+      if (currentGuess === solution) {
+        setIsFinished(true);
+        setCurrentGuess("");
+        return;
+      }
+      setCurrentGuess("");
+    }
+    if (key === "Backspace") {
+      setCurrentGuess(currentGuess.slice(0, currentGuess.length - 1));
+      return;
     }
     if (currentGuess.length === WORD_LENGTH) return;
+    const keyOrd = event.key.toLowerCase().charCodeAt(0);
+    const isLetter = keyOrd >= 65 && keyOrd <= 123;
+    if (!isLetter) return;
     setCurrentGuess(currentGuess + key);
   };
 
@@ -53,6 +85,8 @@ const App = () => {
             <Line
               key={idx}
               guess={isCurrentGuess ? currentGuess : guess ?? ""}
+              isEntered={!isCurrentGuess && guess}
+              solution={solution}
             />
           );
         })}
