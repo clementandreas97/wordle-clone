3fe9e5cb8f7e3411aae1ffd533eb384c75f00b4a
Christopher Clement Andreas <clement.andreas97@gmail.com>
2022-07-04 22:25:41 +0800

    fix: handleType submit not working due to stale state

--- src/App.js
+++ src/App.js
@@ -42,43 +42,54 @@ const App = () => {
   // can be adding currentGuess to the dependency array, but this means
   // the event listener will get added and removed everytime the currentGuess
   // changed.
-  const handleType = useCallback((event) => {
-    if (isFinished) {
-      return;
-    }
-    const { key } = event || {};
-    if (key === 'Enter') {
-      if (currentGuess.length !== WORD_LENGTH) return;
-      const newGuesses = [...guesses];
-      const insertIdx = newGuesses.findIndex((val) => val === null);
-      newGuesses[insertIdx] = currentGuess;
-      setGuesses(newGuesses);
-      if (currentGuess === solution) {
-        setIsFinished(true);
-        setCurrentGuess('');
+  const handleType = useCallback(
+    (event) => {
+      if (isFinished) {
         return;
       }
-      setCurrentGuess('');
-    }
-    if (key === 'Backspace') {
-      setCurrentGuess(currentGuess.slice(0, currentGuess.length - 1));
-      return;
-    }
-    if (currentGuess.length === WORD_LENGTH) return;
-    const keyOrd = event.key.toLowerCase().charCodeAt(0);
-    const isLetter = keyOrd >= 65 && keyOrd <= 123;
-    if (!isLetter) return;
-    // Since we want this function to only created once thus the empty deps array
-    // with the addition of useCallback, we need to have a way to reference the old
-    // currentGuess. Since if we don't do it this way, then it means the function will
-    // refer the currentGuess when it is first created.
-    setCurrentGuess((oldCurrentGuess) => oldCurrentGuess + key);
-  }, []);
+      const { key } = event || {};
+      if (key === 'Enter') {
+        // currentGuess was always referring to the old value when it is not
+        // specified in the dependency array, since this function will only
+        // be initialised once, the first time it's being run.
+        if (currentGuess.length !== WORD_LENGTH) return;
+        const newGuesses = [...guesses];
+        const insertIdx = newGuesses.findIndex((val) => val === null);
+        newGuesses[insertIdx] = currentGuess;
+        setGuesses(newGuesses);
+        console.log(newGuesses);
+        if (currentGuess === solution) {
+          setIsFinished(true);
+          setCurrentGuess('');
+          return;
+        }
+        setCurrentGuess('');
+      }
+      if (key === 'Backspace') {
+        setCurrentGuess((oldCurrentGuess) => oldCurrentGuess.slice(0, oldCurrentGuess.length - 1));
+        return;
+      }
+      if (currentGuess.length >= WORD_LENGTH) return;
+      const keyOrd = event.key.toLowerCase().charCodeAt(0);
+      const isLetter = keyOrd >= 65 && keyOrd <= 123;
+      if (!isLetter) return;
+      // Since we want this function to only created once thus the empty deps array
+      // with the addition of useCallback, we need to have a way to reference the old
+      // currentGuess. Since if we don't do it this way, then it means the function will
+      // refer the currentGuess when it is first created.
+      setCurrentGuess((oldCurrentGuess) => oldCurrentGuess + key);
+    },
+    [currentGuess]
+  );
 
   useEffect(() => {
     window.addEventListener('keydown', handleType);
+    console.log('add listener');
 
-    return () => window.removeEventListener('keydown', handleType);
+    return () => {
+      console.log('remove listener');
+      window.removeEventListener('keydown', handleType);
+    };
   }, [handleType]);
 
   return (
